7850a7672a6280b76f9658712b6237a395d89ab3
crforrester <60713257+crforrester@users.noreply.github.com>
2020-06-08 21:00:31 -0700

    Update Finding Habitable TOI's.py
    Made some corrections to the file where variables such as P_rad were being used before they defined

--- Finding Habitable TOI's.py
+++ Finding Habitable TOI's.py
@@ -14,20 +14,19 @@ P_Insol = exofop['Planet Insolation (Earth Flux)']
 exofop.loc[(P_Insol >= 0.5) & (P_Insol <= 1.5)]
 
 #Planets with an earth radius of 0.5-2.5
-E_Rad = exofop['Planet Radius (R_Earth)']
+P_Rad = exofop['Planet Radius (R_Earth)']
 exofop.loc[(E_Rad >= 0.5) & (E_Rad <= 2.5)]
 
 #Planets with both conditions
-exofop.loc[(P_Insol >= 0.5) & (P_Insol <= 1.5) & (E_Rad >= 0.5) & (E_Rad <= 2.5)]
+exofop.loc[(P_Insol >= 0.5) & (P_Insol <= 1.5) & (P_Rad >= 0.5) & (P_Rad <= 2.5)]
 
 #Zink and Hansen (2019) parameters
+Sol_Rad = exofop['Stellar Radius (R_Sun)']
 Log = exofop['Stellar log(g) (cm/s^2)']
 Depth = exofop['Depth (ppm)']
 Temp = exofop['Stellar Eff Temp (K)']
 exofop.loc[(Temp >= 4200) & (Temp <= 6100) & (Sol_Rad <= 2) & (Log >= 4) & (Depth <= 1000) & (P_Rad >= 0.72) & (P_Rad <= 1.7)]
 
 #Hsu et al (2020) parameters
-P_Rad = exofop['Planet Radius (R_Earth)']
-Sol_Rad = exofop['Stellar Radius (R_Sun)']
 OP = exofop['Period (days)']
 exofop.loc[(P_Rad >= 0.75) & (P_Rad <= 1.5) & (Sol_Rad >= 0.75) & (OP >= 237) & (OP <= 500)]
